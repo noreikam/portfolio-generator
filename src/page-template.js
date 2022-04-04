8da4a2aec127230f0b261c415591fe6710066dc4
noreikam <noreikam1@gmail.com>
2022-04-03 23:26:15 -0400

    finished website being pushed to main

--- src/page-template.js
+++ src/page-template.js
@@ -1,81 +1,69 @@
-// create about section
+// create the about section
 const generateAbout = aboutText => {
-  if(!aboutText) {
-    return ``;
+  if (!aboutText) {
+    return '';
   }
 
-  // create projects
-  const generateProjects = projectsArr => {
-    return `
-    <section class="my-3" id="portfolio">
-    <h2 class="flex-row justify-space-between">Work</h2>
-    <div class="flex-row justify-space-between">
-      <!-- temporarily empty-->
-    </div>
-  </section>
-    `;
-  };
-
   return `
-  <section class="my-3" id="about">
-    <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-    <p>${aboutText}</p>
-  </section>
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
   `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
   return `
     <section class="my-3" id="portfolio">
       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
       <div class="flex-row justify-space-between">
-        ${projectsArr
-            .filter(({ feature }) => feature)
-            .map(({ name, description, languages, link }) =>
-            return `
-            <div class="col-12 mb-2 bg-dark text-light p-3 flex-column ">
-              <h3 class="portfolio-item-title text-light">${name}</h3>    
-              <h5 class="portfolio-languages">
-                Built With:
-                ${languages.join(', ')}      
-              </h5>
-              <p>${description}</p>
-              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-            </div>
-            `;
+      ${projectsArr
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.map(language => language).join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
         })
         .join('')}
 
-        ${projectsArr
-          .filter(({ feature }) => !feature)
-          .map(({ name, description, languages, link }) =>
+      ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
           return `
-          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column ">
-            <h3 class="portfolio-item-title text-light">${name}</h3>    
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.join(', ')}      
+              ${languages.join(', ')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
-          ';
+        `;
         })
         .join('')}
       </div>
-    </section>  
+    </section>
   `;
 };
 
+// export function to generate entire page
 module.exports = templateData => {
-  console.log(templateData);
-
+  // destructure page data by section
   const { projects, about, ...header } = templateData;
 
   return `
   <!DOCTYPE html>
   <html lang="en">
-  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -84,7 +72,6 @@ module.exports = templateData => {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-
   </head>
   
   <body>
